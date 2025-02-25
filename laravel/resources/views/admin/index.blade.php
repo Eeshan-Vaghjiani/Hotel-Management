18146b2c1b1b41c314391ac0f405cd49fba06a87
Eeshan-vaghjiani <evaghjiani@gmail.com>
2025-02-25 16:17:49 +0300

    admin-template

--- laravel/resources/views/admin/index.blade.php
+++ laravel/resources/views/admin/index.blade.php
@@ -1,24 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>ADMIN</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('ADMIN Dashboard') }}
-        </h2>
-    </x-slot>
-
-
-    </div>
-</x-app-layout>
-
-</body>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    @include('admin.header')
+    @include('admin.sidebar')
+    @include('admin.footer')
+    @include('admin.footer')
+  </body>
 </html>
