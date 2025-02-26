db63d5d7d0c47dff8649e71c34c72ad53cc7670a
Eeshan-vaghjiani <evaghjiani@gmail.com>
2025-02-26 10:45:37 +0300

    admin-sidebar

--- laravel/resources/views/admin/sidebar.blade.php
+++ laravel/resources/views/admin/sidebar.blade.php
@@ -14,7 +14,7 @@
                 <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
                 <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Hotel Room</a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                    <li><a href="#">Add Rooms </a></li>
+                    <li><a href="{{ url('create_room') }}">Add Rooms </a></li>
                     <li><a href="#">Page</a></li>
                     <li><a href="#">Page</a></li>
                   </ul>
